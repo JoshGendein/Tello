2e903cb69095b169f55a4c3d53d624e0b2e6c2f8
Josh <joshua@gendein.com>
2019-04-08 13:28:52 -0700

    added video support

--- src/TelloVideo.py
+++ src/TelloVideo.py
@@ -1,3 +1,6 @@
+import cv2
+import threading
+
 class TelloVideo:
     def __init__(self):
         self.udp_video_addr = 'udp://@' + '0.0.0.0' + ':' + str(11111)
@@ -5,3 +8,49 @@ class TelloVideo:
         #VideoCapture object
         self.cap = None
         self.frame_reader = None
+
+    def get_video_capture(self):
+        if self.cap is None:
+            self.cap = cv2.VideoCapture(self.udp_video_addr)
+        
+        if not self.cap.isOpened():
+            self.cap.open(self.udp_video_addr)
+        
+        return self.cap
+    
+    def get_frame_read(self):
+        if self.frame_reader is None:
+            self.frame_reader = FrameReader(self, self.udp_video_addr).start()
+        
+        return self.frame_reader
+    
+    def stop(self):
+        if self.frame_reader is not None:
+            self.frame_reader.stop()
+        if self.cap is not None:
+            self.cap.release()
+
+class FrameReader:
+    def __init__(self, tello_video, address):
+        tello_video.cap = cv2.VideoCapture(address)
+        self.cap = tello_video.cap
+
+        if not self.cap.isOpened():
+            self.cap.open(address)
+        
+        self.grabbed, self.frame = self.cap.read()
+        self.stopped = False
+
+    def start(self):
+        threading.Thread(target=self.update_frame, args = ()).start()
+        return self
+
+    def update_frame(self):
+        while not self.stopped:
+            if not self.grabbed or not self.cap.isOpened():
+                self.stop()
+            else:
+                (self.grabbed, self.frame) = self.cap.read()
+    
+    def stop(self):
+        self.stopped = True
--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import pygame
+import cv2
+import time
 from TelloControl import TelloControl
+from TelloVideo import TelloVideo
 
 a = 0
 b = 0
 c = 0
 d = 0
 speed = 60
+FPS = 25
 
 if __name__ == '__main__':
     drone = TelloControl()
@@ -13,21 +17,28 @@ if __name__ == '__main__':
     drone.streamon()
 
     pygame.init()
-    pygame.display.set_mode((500, 500))
-    while True:
+    pygame.display.set_caption("Tello video stream")
+    screen = pygame.display.set_mode([960, 720])
+
+    drone_video = TelloVideo()
+
+    frame_read = drone_video.get_frame_read()
+
+    should_stop = False
+    while not should_stop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 drone.stop()
+                drone_video.stop()
                 pygame.quit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     drone.stop()
+                    drone_video.stop()
                     pygame.quit()
                 elif event.key == pygame.K_SPACE:
-                    print('space')
                     drone.takeoff()
                 elif event.key == pygame.K_BACKSPACE:
-                    print('back-space')
                     drone.land()
                 elif event.key == pygame.K_a:
                     a = -speed
@@ -45,7 +56,6 @@ if __name__ == '__main__':
                     d = speed
                 elif event.key == pygame.K_LEFT:
                     d = -speed
-                # print('Sending RC command: ', (a, b, c, d))
                 drone.send_rc((a, b, c, d))                 
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_a or event.key == pygame.K_d:
@@ -56,7 +66,22 @@ if __name__ == '__main__':
                     c = 0
                 elif event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     d = 0
-                # print('Sending RC command: ', (a, b, c, d))
                 drone.send_rc((a, b, c, d))
+
+        if frame_read.stopped:
+            frame_read.stop()
+            break
+        
+        self.screen.fill([0, 0, 0])
+        frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2RGB)
+        frame = np.rot90(frame)
+        frame = np.flipud(frame)
+        frame = pygame.surfarray.make_surface(frame)
+        screen.blit(frame, (0, 0))
+        pygame.display.update()
+
+        time.sleep(1 / FPS)
+        
     print('Drone is stopping...')
     drone.stop()
+    drone_video.stop()
