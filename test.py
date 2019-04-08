4749a4c153e12082f4ab2dc3e06ac02ea8412e54
JoshRTYUPROE <jgendein@gmail.com>
2019-04-08 09:58:34 -0700

    changed test to use keyboard

--- test.py
+++ test.py
@@ -1,26 +1,14 @@
-from pynput import keyboard
+import keyboard
 
 def keyboard_down(input):
-    try:
-        key = input.char
-    except AttributeError:
-        key = input
-    if key == 't':
-        return True
+    print('Down: ', input.name)
 
 def keyboard_release(input):
-    try:
-        key = input.char
-    except AttributeError:
-        key = input
-    if key == 't':
-        print(key)
-    elif key == keyboard.Key.esc:
-        return False
+    print('Release: ', input.name)
 
 if __name__ == "__main__":
 
-    with keyboard.Listener(
-            on_press=keyboard_down,
-            on_release=keyboard_release) as listener:
-        listener.join()
+    keyboard.on_press(keyboard_down)
+    keyboard.on_release(keyboard_release)
+
+    keyboard.wait('esc')
